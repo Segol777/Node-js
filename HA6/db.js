0255940de9bbc50a5fe1ec1b55e619894d0d3222
Armagedon <grauberger1@web.de>
2025-01-11 15:09:18 +0100

    Add files via upload

---
+++ HA6/db.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  // host: "localhost",
+  // user: "root",
+  // password: "pw",
+  // database: "product_db",
+  host:'localhost',
+  database:'execute',
+  user:'kontrolle',
+  password: '7777777',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error to connect to Database", err.stack);
+    return;
+  }
+  console.log("Conected to Database");
+});
+
+export default connection;
